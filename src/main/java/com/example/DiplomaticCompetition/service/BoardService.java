0e0a2f2dc8e4e80244895dd05aea658c01022c91
changeme4585 <benscience@naver.com>
2025-06-23 15:42:38 +0900

    feat: 게시판 목록 조회 서비스

---
+++ src/main/java/com/example/DiplomaticCompetition/service/BoardService.java
@@ -0,0 +1,37 @@
+package com.example.DiplomaticCompetition.service;
+
+
+import com.example.DiplomaticCompetition.domain.Board;
+import com.example.DiplomaticCompetition.dto.response.BoardListDto;
+import com.example.DiplomaticCompetition.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+    public List<BoardListDto>getBoardList(int offset){
+        PageRequest pageRequest = PageRequest.of(offset / 10, 10); // offset부터 10개
+        List<BoardListDto> boardListDtos = new ArrayList<>();
+        List<Board>  boarList = boardRepository.findBoardWithOffset(offset, pageRequest);
+        for (Board board : boarList){
+            BoardListDto boardListDto = BoardListDto.builder()
+                    .boardId(board.getId()).
+                    nickName(board.getNickName()).
+                    likeCount(board.getLikeCount()).
+                    title(board.getTitle()).
+                    build();
+            boardListDtos.add(boardListDto);
+        }
+        return boardListDtos;
+    }
+
+}
