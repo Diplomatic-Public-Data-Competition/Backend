4ce6cf6e5d7eb08dceb9922defa385af34d2513c
changeme4585 <benscience@naver.com>
2025-06-23 11:12:39 +0900

    Initial commit for DiplomaticCompetition

---
+++ src/main/java/com/example/DiplomaticCompetition/DiplomaticCompetitionApplication.java
@@ -0,0 +1,13 @@
+package com.example.DiplomaticCompetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaticCompetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomaticCompetitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DiplomaticCompetition/DiplomaticCompetitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DiplomaticCompetition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaticCompetitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
