f1574ee0e06c15700952cdccf7ffd28ec125c78e
changeme4585 <benscience@naver.com>
2025-06-23 16:04:54 +0900

    refactor: 앤드포인트 수정

--- src/main/java/com/example/DiplomaticCompetition/controller/BoardController.java
+++ src/main/java/com/example/DiplomaticCompetition/controller/BoardController.java
@@ -6,10 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/board")
+@RestController
+@RequestMapping("/api/board")
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
