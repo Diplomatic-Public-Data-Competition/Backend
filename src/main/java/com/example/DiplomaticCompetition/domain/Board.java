99a16c133032940c25e013227b90703d9813a135
changeme4585 <benscience@naver.com>
2025-06-23 15:21:41 +0900

    Refactor: rename

--- src/main/java/com/example/DiplomaticCompetition/domain/Board.java
+++ src/main/java/com/example/DiplomaticCompetition/domain/Board.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class board {
+public class Board {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
