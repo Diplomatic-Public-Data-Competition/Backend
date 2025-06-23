f1ee5582da2557d3ec084b7c8f7be19cbc3e2b35
changeme4585 <benscience@naver.com>
2025-06-23 14:01:40 +0900

    feat: 게시판 엔티티

---
+++ src/main/java/com/example/DiplomaticCompetition/domain/board.java
@@ -0,0 +1,44 @@
+package com.example.DiplomaticCompetition.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; // 기본키
+
+    @Column
+    private String title; // 제목
+
+    @Column
+    private String content; // 본문
+
+    @Column
+    private String nickName; //  닉네임
+
+    @Column // 게시글 좋아요 수
+    private int likeCount;
+
+
+    public void onLikeCnt() {//좋아요 수 증가
+        this.likeCount += 1;
+    }
+
+    public void updateBoard(String title, String content) { //게시글 수정
+        this.title = title;
+        this.content = content;
+
+    }
+
+}
