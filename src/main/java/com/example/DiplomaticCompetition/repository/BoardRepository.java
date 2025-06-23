47f85ef21b2858db535c67222c8adaf7b86a90a7
changeme4585 <benscience@naver.com>
2025-06-23 15:51:37 +0900

    refactor: 문법수정

--- src/main/java/com/example/DiplomaticCompetition/repository/BoardRepository.java
+++ src/main/java/com/example/DiplomaticCompetition/repository/BoardRepository.java
@@ -2,6 +2,7 @@ package com.example.DiplomaticCompetition.repository;
 
 import com.example.DiplomaticCompetition.domain.Board;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface BoardRepository {
-    @Query(value = "SELECT b FROM board b ")
+public interface BoardRepository extends JpaRepository<Board, Long> {
+    @Query(value = "SELECT b FROM Board b ")
     List<Board> findBoardWithOffset(@Param("offset") int offset, org.springframework.data.domain.Pageable pageable);
 }
