f51573c22a762d8d40cbfb773855634fc576ce82
changeme4585 <benscience@naver.com>
2025-06-23 16:18:08 +0900

    feat: 게시판 Id로 조회

--- src/main/java/com/example/DiplomaticCompetition/repository/BoardRepository.java
+++ src/main/java/com/example/DiplomaticCompetition/repository/BoardRepository.java
@@ -8,9 +8,12 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
     @Query(value = "SELECT b FROM Board b ")
     List<Board> findBoardWithOffset(@Param("offset") int offset, org.springframework.data.domain.Pageable pageable);
+
+    Optional<Board> findById(Long id);
 }
